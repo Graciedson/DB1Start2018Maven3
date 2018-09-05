18d99d55b132c2ee3b5d8bdd1871d92b60e04f1d
Graciedson <graciedson27@gmail.com>
2018-09-05 17:00:38 -0300

    DAO

---
+++ src/main/java/br/com/db1/dao/impl/UfDao.java
@@ -0,0 +1,42 @@
+package br.com.db1.dao.impl;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.db1.dao.DAO;
+import br.com.db1.model.Uf;
+
+public class UfDao implements DAO <Uf>{
+	
+	@Inject
+	EntityManager manager;
+
+	public List<Uf> findAll() {
+		
+		return manager.createQuery("from uf").getResultList();
+	}
+
+	public Uf findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Uf> findByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean save(Uf t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean delete(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
